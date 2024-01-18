7cdad635ca959835ade4a58e227fec319a10adf1
Andrew Shum <andrew.shum068@gmail.com>
2024-01-18 07:26:25 -0500

    descriptions

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,12 +2,38 @@ import React from 'react'
 
 const Experience = () => {
     return (
-        <div id='experience' className="grid grid-cols-3 shadow p-14 rounded-3xl mt-8">
-            <div className='col-span-2'>
-                <h1 className='font-extrabold text-5xl bg-gradient-to-r from-purple-500 via-indigo-400 to-blue-400 inline-block text-transparent bg-clip-text'>Experience</h1>
-                <p>I am currently studying Computer Science at the University of Waterloo.</p>
-                <p>I </p>
+        <div id='experience' className="shadow p-14 rounded-3xl mt-8">
+            <h1 className='font-extrabold text-5xl bg-gradient-to-r from-purple-500 via-indigo-400 to-blue-400 inline-block text-transparent bg-clip-text'>Experience</h1>
+            <div className='mt-8 ml-4'>
+                <h3>President</h3>
+                <h4>Science Club</h4>
+                <p>description</p>
             </div>
+            <hr />
+            <div className='mt-8 ml-4'>
+                <h3>Vice President</h3>
+                <h4>STEM Without Boundaries -- Toronto Chapter</h4>
+                <p>description</p>
+            </div>
+            <hr />
+            <div className='mt-8 ml-4'>
+                <h3>Chapter Vice President</h3>
+                <h4>Key Club</h4>
+                <p>description</p>
+            </div>
+            <hr />
+            <div className='mt-8 ml-4'>
+                <h3>Turf Maintenance Technician</h3>
+                <h4>Station Creek Golf Club</h4>
+                <p>description</p>
+            </div>
+            <hr />
+            <div className='mt-8 ml-4'>
+                <h3>Hockey Coaching Assistant</h3>
+                <h4>Markham Waxers </h4>
+                <p>description</p>
+            </div>
+            
 
         </div>
     )
