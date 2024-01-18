e4650af19578c0b955d36286a72155c07ecd7816
Andrew Shum <andrew.shum068@gmail.com>
2024-01-18 02:56:02 -0500

    updates

---
+++ public/src/App.js
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Landing, Projects, Navbar } from './components'
+
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      
+      <Landing />
+      <Navbar />
+      <Projects />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ public/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Landing } from './Landing';
+export { default as Projects } from './Projects';
+export { default as Navbar } from './Navbar';
