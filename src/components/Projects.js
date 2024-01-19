de8d483dff3053dbea1af992e7cdf3781474d0f9
Andrew Shum <andrew.shum068@gmail.com>
2024-01-19 17:16:00 -0500

    Updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 import placeholder from '../assets/placeholder.jpg'
 
+import pw from '../assets/pw.png'
+import bsm from '../assets/bsm.png'
+
 const Projects = () => {
   return (
     <div id='projects'>
@@ -14,11 +17,11 @@ const Projects = () => {
             <p>A Blind Spot Monitor for driving by leveraging the power of OpenCV in conjunction with the state-of-the-art
               YOLO Object Detection model to classify vehicle types to successfully enhance safety by detecting hazards.</p>
             <p>Built with: Python, OpenCV, YOLO Object Detection Algorithm</p>
-            <p>Technical Overview</p>
+            
             <Button href='https://github.com/ashum68/blindspotmonitor' variant='dark' target="_blank" rel="noopener noreferrer">GitHub</Button>
           </div>
           <div className='col-span-5 lg:col-span-2 p-3'>
-            <img src={placeholder} alt='project picture' className='w-full h-full rounded-xl' />
+            <img src={bsm} alt='project picture' className='w-full h-full rounded-xl' />
 
           </div>
         </div>
@@ -27,7 +30,7 @@ const Projects = () => {
             <h2>Stock-Tracking Discord Bot</h2>
             <p>The Stalk-a-Stock Discord Bot is capable of outputting real-time stock prices by using Slash Commands and requesting any stock ticker.</p>
             <p>Built with: JavaScript, REST API, discord.js</p>
-            <p>Technical Overview: Stock data is retrieved using the Financial Modelling Prep REST API.</p>
+            
             <Button href='https://github.com/ashum68/stalkstock' variant='dark' target="_blank" rel="noopener noreferrer">GitHub</Button>
           </div>
           <div className='col-span-5 lg:col-span-2 p-3'>
@@ -39,11 +42,11 @@ const Projects = () => {
             <h2>Personal Portfolio Website</h2>
             <p>This is my personal portfolio website where I can share all of my creative projects, my experiences, and a little bit about myself and my journey to becoming a software engineer.</p>
             <p>Built with: React, JavaScript, HTML, TailwindCSS</p>
-            <p>Technical Overview</p>
+            
             <Button href='https://github.com/ashum68/ashum68.github.io' variant='dark' target="_blank" rel="noopener noreferrer">GitHub</Button>
           </div>
           <div className='col-span-5 lg:col-span-2 p-3'>
-            <img src={placeholder} alt='project picture' className='w-full h-full rounded-xl' />
+            <img src={pw} alt='project picture' className='w-full h-full rounded-xl' />
           </div>
         </div>
       </div>
